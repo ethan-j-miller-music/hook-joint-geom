bc00f18d983ccb8156b7adf6fb1062fe33b70bec
ethan-j-miller-music <233996328+ethan-j-miller-music@users.noreply.github.com>
2026-02-05 15:37:11 +0800

    Add procedural API and factory function for Hooke joint creation

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .api import *
+
+__all__ = [
+    # OOP
+    "create_joint",
+    # procedural (the 4 functions)
+    "direction_from_angles",
+    "angles_from_direction",
+    "rope_lengths_from_angles",
+    "rope_lengths_from_direction",
+]
